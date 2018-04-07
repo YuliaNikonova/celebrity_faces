9f7844012403eceb8cffe181f9fdef3603957482
YuliaNikonova <nikonova.julya@gmail.com>
2018-04-07 22:01:14 +0300

    get path of closest celeb for image

--- image_processing.py
+++ image_processing.py
@@ -24,23 +24,58 @@
 
 
 import cv2
+from skimage.transform import resize
+import numpy as np
+from os.path import expanduser, join, abspath
+import tensorflow as tf
 import sys
-#import matplotlib.pyplot as plt
-from os.path import expanduser, abspath, join
-FACENET_SRC = abspath(join(expanduser('~')), 'repos', 'facenet', 'src')
+
+FACENET_SRC_DIR = abspath(join(expanduser('~'), 'repos', 'facenet', 'src'))
+sys.path.append(FACENET_SRC_DIR)
+from facenet import get_model_filenames
+
+
+
+# from facenet
+def prewhiten(x):
+    mean = np.mean(x)
+    std = np.std(x)
+    std_adj = np.maximum(std, 1.0/np.sqrt(x.size))
+    y = np.multiply(np.subtract(x, mean), 1/std_adj)
+    return y
+
 
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 
-'''
-for i in range(nrof_samples):
-        img = misc.imread(image_paths[i])
-        if img.ndim == 2:
-            img = to_rgb(img)
-        if do_prewhiten:
-            img = prewhiten(img)
-        img = crop(img, do_random_crop, image_size)
-        img = flip(img, do_random_flip)
-        images[i,:,:,:] = img
-    return images
-'''
+
+def prepocess_image(img, image_size=160):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    scale = 0.15
+    for (x, y, w, h) in faces:
+        x_min = x - int(scale * w)
+        y_min = y - int(scale * h)
+        x_max = x + w + int(scale * w)
+        y_max = y + h + int(scale * h)
+    img = prewhiten(img)
+    img = img[x_min:x_max, y_min:y_max, :]
+    img = resize(img, (image_size, image_size))
+    return img
+
+
+def get_tf_session(model_directory):
+    sess = tf.Session()
+    meta_file, ckpt_file = get_model_filenames(model_directory)
+    saver = tf.train.import_meta_graph(join(model_directory, meta_file))
+    saver.restore(sess, join(model_directory, ckpt_file))
+    images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+    embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+    phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+    return sess, images_placeholder, embeddings, phase_train_placeholder
+
+
+def get_vector(image, sess, images_placeholder, embeddings, phase_train_placeholder):
+    feed_dict = {images_placeholder: [image], phase_train_placeholder: False}
+    return sess.run(embeddings, feed_dict=feed_dict)[0]
+
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from annoy import AnnoyIndex
 import numpy as np
+from scipy import misc
+from os.path import expanduser, join, abspath
+MODEL_DIR = abspath(join(expanduser('~'), 'models', 'facenet', '20170512-110547'))
+from image_processing import prepocess_image, get_tf_session, get_vector
 
 INDEX_FILENAME = 'data/index.ann'
 PATHS_NPY = 'data/paths.npy'
 EMBEDDING_NPY = 'data/embeddings.npy'
+
+PATHS = np.load(PATHS_NPY)
 
 
 def build_index(embeddings):
@@ -29,6 +35,21 @@ def build_index(embeddings):
 def main():
     embeddings = np.load(EMBEDDING_NPY)
     index = build_index(embeddings)
+
+    test_filename = abspath(join(expanduser('~'), 'datasets', 'lfw', 'raw', 'Aaron_Eckhart', 'Aaron_Eckhart_0001.jpg'))
+    image = misc.imread(test_filename)
+    img_preprocessed = prepocess_image(image)
+
+    sess, images_placeholder, embeddings, phase_train_placeholder = get_tf_session(MODEL_DIR)
+    try:
+        vector = get_vector(img_preprocessed, sess, images_placeholder, embeddings, phase_train_placeholder)
+
+        closest_celeb_index = index.get_nns_by_vector(vector, 1)[0]
+        closest_celeb_path = PATHS[closest_celeb_index]
+        print(closest_celeb_path)
+    finally:
+        sess.close()
+
 
 
 if __name__ == "__main__":
