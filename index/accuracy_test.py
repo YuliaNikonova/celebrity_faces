b3908a5d90365cb037bbabbfa6cff1bb830b7616
YuliaNikonova <nikonova.julya@gmail.com>
2018-05-13 20:36:12 +0100

    add nsw index to index.py & accuracy_test.py

--- index/accuracy_test.py
+++ index/accuracy_test.py
@@ -17,8 +17,17 @@ from __future__ import print_function
 import h5py
 import unittest
 import os
-from os.path import dirname, join, abspath
+from os.path import dirname, join, abspath, exists
 from annoy import AnnoyIndex
+try:
+    from urllib import urlretrieve
+except ImportError:
+    from urllib.request import urlretrieve # Python 3
+import gzip
+from nose.plugins.attrib import attr
+
+from python.index import PyNode, PyDistance_l1, PyDistance_l2, PyNSW
+from tqdm import tqdm, trange
 
 
 class AccuracyTest(unittest.TestCase):
@@ -26,19 +35,29 @@ class AccuracyTest(unittest.TestCase):
         vectors_fn = join(os.getenv('ACCURACY_TEST_DATA_PATH',
                abspath(join(abspath(dirname(abspath(dirname(__file__)))), 'data', 'test'))), dataset + '.hdf5')
 
+        if not exists(vectors_fn):
+            url = 'http://vectors.erikbern.com/%s.hdf5' % dataset
+            print('downloading', url, '->', vectors_fn)
+            urlretrieve(url, vectors_fn)
 
-        index_fn = os.path.join('test', dataset + '.annoy')
+        index_fn = os.path.join(dataset + '.annoy')
         dataset_f = h5py.File(vectors_fn)
         distance = dataset_f.attrs['distance']
         f = dataset_f['train'].shape[1]
         annoy = AnnoyIndex(f, distance)
+
+        print('building nsw index')
+        nsw = PyNSW('l2')
+        for i in trange(dataset_f['train'].shape[0]):
+            v = dataset_f['train'][i]
+            nsw.nn_insert(PyNode(str(i), v), 1, 1000)
 
         if not os.path.exists(index_fn):
             print('adding items', distance, f)
             for i, v in enumerate(dataset_f['train']):
                 annoy.add_item(i, v)
 
-            print('building index')
+            print('building annoy index')
             annoy.build(10)
             annoy.save(index_fn)
         else:
@@ -48,27 +67,33 @@ class AccuracyTest(unittest.TestCase):
     def _test_index(self, dataset, exp_accuracy):
         annoy, dataset_f = self._get_index(dataset)
 
-        n, k = 0, 0
+        n, k_annoy, k_nsw = 0, 0, 0
 
         for i, v in enumerate(dataset_f['test']):
             js_fast = annoy.get_nns_by_vector(v, 10, 1000)
             js_real = dataset_f['neighbors'][i][:10]
+            js_fast_nsw = [int(result[1]) for result in nsw.nn_search(PyNode('1', v), 1, 10)]
+
             assert len(js_fast) == 10
             assert len(js_real) == 10
+            assert len(js_fast_nsw) == 10
 
             n += 10
-            k += len(set(js_fast).intersection(js_real))
+            k_annoy += len(set(js_fast).intersection(js_real))
+            k_nsw += len(set(js_fast_nsw).intersection(js_real))
 
-        accuracy = 100.0 * k / n
-        print('%50s accuracy: %5.2f%% (expected %5.2f%%)' % (dataset, accuracy, exp_accuracy))
+        accuracy_annoy = 100.0 * k_annoy / n
+        accuracy_nsw = 100.0 * k_nsw / n
+        print('Annoy: %50s accuracy: %5.2f%% (expected %5.2f%%)' % (dataset, accuracy_annoy, exp_accuracy))
+        print('NSW: %50s accuracy: %5.2f%%' % (dataset, accuracy_nsw))
 
         self.assertTrue(accuracy > exp_accuracy - 1.0) # should be within 1%
 
-    def test_glove_25(self):
-        self._test_index('glove-25-angular', 69.00)
+    #def test_glove_25(self):
+    #    self._test_index('glove-25-angular', 69.00)
 
-    def test_nytimes_16(self):
-        self._test_index('nytimes-16-angular', 80.00)
+    #def test_nytimes_16(self):
+    #    self._test_index('nytimes-16-angular', 80.00)
 
     def test_fashion_mnist(self):
         self._test_index('fashion-mnist-784-euclidean', 90.00)
--- index/index.py
+++ index/index.py
@@ -5,6 +5,8 @@ from os import getenv
 import json
 import jsonpickle
 
+from python.index import PyNode, PyDistance_l1, PyDistance_l2, PyNSW
+
 INDEX_FILENAME = getenv('INDEX_FILENAME',
                       abspath(join(abspath(dirname(abspath(dirname(__file__)))), 'data', 'index.ann')))
 PATHS_JSON = getenv('PATHS_JSON',
@@ -40,13 +42,25 @@ except FileNotFoundError:
         embedding_index = AnnoyIndex(dim)
         embedding_index.load(INDEX_FILENAME)
 
+nodes = [PyNode(path, vector) for path, vector in zip(PATHS, embeddings)]
+nsw = PyNSW('l2')
+for node in nodes:
+    nsw.nn_insert(node, 1, 1000)
+
+def get_closest_celeb_filename_annoy(vector):
+    closest_celeb_index = embedding_index.get_nns_by_vector(vector, 1)[0]
+    return str(PATHS[closest_celeb_index])
+
+
+def get_closest_celeb_filename_nsw(vector):
+    return nsw.nn_search(PyNode('1', vector), 1, 1)[0][1]
 
 @app.route('/', methods=['POST'])
 def get_closest_vector():
     content = request.get_json(silent=True)
     vector = content['vector']
-    closest_celeb_index = embedding_index.get_nns_by_vector(vector, 1)[0]
-    closest_celeb_filename = str(PATHS[closest_celeb_index])
+    
+    closest_celeb_filename = get_closest_celeb_filename_annoy(vector)
     response = {'closest_celeb_filename': closest_celeb_filename}
     response_pickled = jsonpickle.encode(response)
     return Response(response=response_pickled, status=200, mimetype="application/json")
